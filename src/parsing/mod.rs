5387068dd59a7890ea66dfdb23ee551686c0922d
Seunghyun An <drash99@naver.com>
2022-10-21 06:17:59 +0900

    parse more

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -5,7 +5,9 @@ pub use parser::Parser;
 #[derive(Debug, PartialEq)]
 pub enum ParseLineResult {
     Spawned(String, String, usize, usize),
-    SpawnMatch(usize,usize,usize),
+    SpawnMatch(usize, usize, usize),
+    Unspawn(usize),
+    Success(usize, bool),
     UnhandledGameSession(String),
     Unhandled(String),
     Misc,
--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,17 +1,17 @@
 use super::*;
+use regex::Regex;
 use std::fs::*;
 use std::io::*;
-use regex::Regex;
-
-
 
 #[derive(Debug)]
 pub struct Parser {
     pub file: File,
     pub parsed: Vec<ParseLineResult>,
-    regex_spawn : Regex,
-    regex_spawn2 : Regex,
-    regex_spawn_match : Regex,
+    regex_spawn: Regex,
+    regex_spawn2: Regex,
+    regex_spawn_match: Regex,
+    regex_unspawn: Regex,
+    regex_success: Regex,
 }
 
 impl Parser {
@@ -22,7 +22,8 @@ impl Parser {
             regex_spawn: Regex::new(r"\d{4}-\d{2}-\d{2}: \[\w*\] Finalising spawn for player FallGuy \[(\d*)\] ([^(]*) \((\w*)\)+").unwrap(),
             regex_spawn2: Regex::new(r"\d{4}-\d{2}-\d{2}: \[\w*\] Adding Spectator target \w*_([^(]*) \((\w*)\) with Party ID: ([\d ]*) Squad ID: (\d*) and playerID: (\d*)+").unwrap(),
             regex_spawn_match: Regex::new(r"\d{4}-\d{2}-\d{2}: \[\w*\] Handling bootstrap for \w* player FallGuy \[(\d*)\] \([\w\d.]*\), playerID = (\d*), squadID = (\d*)+").unwrap(),
-
+            regex_unspawn : Regex::new(r"\d{4}-\d{2}-\d{2}: \[\w*\] Handling unspawn for player FallGuy \[(\d*)\]").unwrap(),
+            regex_success : Regex::new(r"\d{4}-\d{2}-\d{2}: ClientGameManager::HandleServerPlayerProgress PlayerId=(\d*) is succeeded=(\w*)").unwrap(),
         }
     }
 
@@ -37,7 +38,7 @@ impl Parser {
     }
 
     fn parse_line(&self, line: String) -> ParseLineResult {
-        if !line.contains(' '){
+        if !line.contains(' ') {
             return ParseLineResult::Misc;
         }
         let mut split = line.split(" ");
@@ -47,40 +48,94 @@ impl Parser {
             "[ClientGameSession]" => ParseLineResult::UnhandledGameSession(line),
             "[ClientGameManager]" => match split.next().unwrap() {
                 "Finalising" => {
-                    let captured = self.regex_spawn.captures(&line).unwrap();
+                    let captured = self
+                        .regex_spawn
+                        .captures(&line)
+                        .unwrap_or_else(|| panic!("Failed to parse line: {}", line));
                     ParseLineResult::Spawned(
-                        captured.get(2).map_or(String::new(), |m| m.as_str().to_string()),
-                        captured.get(3).map_or(String::new(), |m| m.as_str().to_string()), 
-                        captured.get(1).map_or(0, |m| m.as_str().parse::<usize>().unwrap()), 0)
-                },
-                "Handling" => {
-                    if split.next().unwrap() == "bootstrap"{
-                        let captured = self.regex_spawn_match.captures(&line).unwrap_or_else(|| panic!("Failed to parse line: {}", line));
-                        ParseLineResult::SpawnMatch(
-                            captured.get(1).map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
-                            captured.get(2).map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
-                            captured.get(3).map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
-                        )
-                    }else {
-                        ParseLineResult::Unhandled(line)
-                    }
+                        captured
+                            .get(2)
+                            .map_or(String::new(), |m| m.as_str().to_string()),
+                        captured
+                            .get(3)
+                            .map_or(String::new(), |m| m.as_str().to_string()),
+                        captured
+                            .get(1)
+                            .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                        0,
+                    )
                 }
+                "Handling" => match split.next().unwrap() {
+                    "bootstrap" => {
+                        let captured = self
+                            .regex_spawn_match
+                            .captures(&line)
+                            .unwrap_or_else(|| panic!("Failed to parse line: {}", line));
+                        ParseLineResult::SpawnMatch(
+                            captured
+                                .get(1)
+                                .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                            captured
+                                .get(2)
+                                .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                            captured
+                                .get(3)
+                                .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                        )
+                    }
+                    "unspawn" => {
+                        let captured = self
+                            .regex_unspawn
+                            .captures(&line)
+                            .unwrap_or_else(|| panic!("Failed to parse line: {}", line));
+                        ParseLineResult::Unspawn(
+                            captured
+                                .get(1)
+                                .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                        )
+                    }
+                    _ => ParseLineResult::Unhandled(line),
+                },
                 _ => ParseLineResult::Unhandled(line),
             },
             "[CameraDirector]" => match split.next().unwrap() {
                 "Adding" => {
-                    let captured = self.regex_spawn2.captures(&line);
-                    if captured.is_none() {
-                        return ParseLineResult::Unhandled(line);
-                    }
-                    let captured = captured.unwrap();
+                    let captured = self
+                        .regex_spawn2
+                        .captures(&line)
+                        .unwrap_or_else(|| panic!("Failed to parse line: {}", line));
                     ParseLineResult::Spawned(
-                        captured.get(1).map_or(String::new(), |m| m.as_str().to_string()),
-                        captured.get(2).map_or(String::new(), |m| m.as_str().to_string()), 
-                        captured.get(5).map_or(0, |m| m.as_str().parse::<usize>().unwrap()), 
-                        captured.get(4).map_or(0, |m| m.as_str().parse::<usize>().unwrap()))
+                        captured
+                            .get(1)
+                            .map_or(String::new(), |m| m.as_str().to_string()),
+                        captured
+                            .get(2)
+                            .map_or(String::new(), |m| m.as_str().to_string()),
+                        captured
+                            .get(5)
+                            .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                        captured
+                            .get(4)
+                            .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                    )
                 }
                 _ => ParseLineResult::Unhandled(line),
+            },
+            "ClientGameManager::HandleServerPlayerProgress" => {
+                let captured = self
+                    .regex_success
+                    .captures(&line)
+                    .unwrap_or_else(|| panic!("Failed to parse line: {}", line));
+                ParseLineResult::Success(
+                    captured
+                        .get(1)
+                        .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
+                    if captured.get(2).map_or(false, |m| m.as_str() == "True") {
+                        true
+                    } else {
+                        false
+                    },
+                )
             }
             _ => ParseLineResult::Misc,
         }
